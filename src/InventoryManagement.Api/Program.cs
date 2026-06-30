21c79b2297a370794d020b126b24a86609d3e280
suresh-piocon <pioconsoftware@gmail.com>
2026-06-30 16:25:47 +0530

    Configure System.Text.Json to ignore reference cycles during serialization

--- src/InventoryManagement.Api/Program.cs
+++ src/InventoryManagement.Api/Program.cs
@@ -13,9 +13,12 @@ using InventoryManagement.Api.Data;
 using InventoryManagement.Shared;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddHttpContextAccessor();
 
